4b31f4a7a074a5fe7eda9e44d483f6f8c85acaca
zou_luxian <zoulux@126.com>
2019-12-17 17:53:45 +0800

    init

---
+++ src/WenjuanServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Jake\LaravelWenjuan;
+
+use Illuminate\Foundation\Application as LaravelApplication;
+use Jake\Wenjuan\WenJuanClient;
+
+class WenjuanServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        $this->setupConfig();
+
+        $this->app->singleton('wenjuan', function () {
+            $config = config('wenjuan');
+            return new WenJuanClient($config);
+        });
+    }
+
+    /**
+     * Setup the config.
+     */
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/config.php');
+
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('wenjuan.php')], 'laravel-wenjuan');
+        }
+
+        $this->mergeConfigFrom($source, 'wenjuan');
+    }
+}
---
+++ src/config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'app_key' => env('WENJUAN_APP_KEY', 'your-app_key'),
+    'app_secret' => env('WENJUAN_APP_SECRET', 'your-app_secret'),
+];
