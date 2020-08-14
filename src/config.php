ed66b415e79d4bf49040b86e367b7541bc272f11
zou_luxian <zoulux@126.com>
2020-08-14 15:53:33 +0800

    fix(config) add mode timestamp

--- src/config.php
+++ src/config.php
@@ -3,4 +3,6 @@
 return [
     'app_key' => env('WENJUAN_APP_KEY', 'your-app_key'),
     'app_secret' => env('WENJUAN_APP_SECRET', 'your-app_secret'),
+    'mode' => env('WENJUAN_APP_MODE', 'your-app_mode'),
+    'timestamp' => env('WENJUAN_APP_TIMESTAMP', 'your-app_timestamp'),
 ];
